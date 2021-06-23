caf3bfbad7db1de6c0f84bea27767c692486732f
bg308222 <bg308222@gmail.com>
2021-06-23 17:28:05 +0800

    Fix animation

--- src/common/component/MyButton/MyButton.tsx
+++ src/common/component/MyButton/MyButton.tsx
@@ -1,18 +1,41 @@
+import { useState } from "react";
 import "./MyButton.scss";
+
+interface IButtonStyle {
+  color?: string;
+  backGroundColor?: string;
+  borderColor?: string;
+}
 
 export const MyButton: React.FC<{
   text: string;
   onClick?: () => void;
   className?: string;
   style?: React.HTMLAttributes<HTMLDivElement>["style"];
-}> = ({ text, onClick, className, style }) => {
+  hoverButtonStyle?: IButtonStyle;
+}> = ({ text, onClick, className, style, hoverButtonStyle }) => {
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <div
+      onMouseEnter={() => {
+        setIsHover(true);
+      }}
+      onMouseLeave={() => {
+        setIsHover(false);
+      }}
       className={`MyButton ${className || ""}`}
       onClick={() => {
         onClick && onClick();
       }}
-      style={style}
+      style={{
+        ...style,
+        color: isHover
+          ? hoverButtonStyle
+            ? hoverButtonStyle.color
+            : "#30daa2"
+          : undefined,
+      }}
     >
       {text}
     </div>
